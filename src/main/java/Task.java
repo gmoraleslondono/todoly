25a1dabee33847cde4331f8a4e5e7ae99b4ec7ff
Gloria Morales <gmoraleslondono@gmail.com>
2018-09-24 00:57:31 +0200

    Add test task has a due date method

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,5 +1,11 @@
+import java.time.LocalDate;
+
 public class Task {
     public String getTitle() {
         return "Hello world.";
     }
+
+	public LocalDate getDueDate() {
+		return LocalDate.now();
+	}
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,10 +1,17 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.time.LocalDate;
 
 public class TaskTest {
-    @Test 
+    @Test
     public void testTaskHasTitle() {
         Task task = new Task();
         assertTrue("task should have a title", task.getTitle() instanceof String);
+    }
+
+    @Test
+    public void testTaskHasDueDate() {
+        Task task = new Task();
+        assertTrue("task should have a due date", task.getDueDate() instanceof LocalDate);
     }
 }
